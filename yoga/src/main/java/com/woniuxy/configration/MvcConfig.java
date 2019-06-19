4432ed86182bbe34d90f172a17d5cc862d6eabec
jiaweihang <jiaweihang0313@163.com>
2019-06-19 10:48:18 +0800

    first commit

--- yoga/src/main/java/com/woniuxy/configration/MvcConfig.java
+++ yoga/src/main/java/com/woniuxy/configration/MvcConfig.java
@@ -8,6 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        //registry.addViewController("/welcome").setViewName("login");
+        registry.addViewController("/welcome").setViewName("login");
     }
 }
